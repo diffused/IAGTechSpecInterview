0eb8b1186c90ba2ae0dc562069dbcbef5776e8a1
David Theunissen <davetheunissen@gmail.com>
2018-10-16 15:18:26 +1300

    create initial tests for candidate to refactor

--- src/IAGTechSpecInterview.Tests/LoggerTests/LogShould.cs
+++ src/IAGTechSpecInterview.Tests/LoggerTests/LogShould.cs
@@ -9,17 +9,13 @@ namespace IAGTechSpecInterview.Tests.LoggerTests
     public class LogShould
     {
         /// <summary>
-        /// Please refactor the sut FourWallsInterview.Logger and verify the 
+        /// Please refactor the sut IAGTechSpecInterview.Logger and verify the 
         /// Log method is being called correctly
         /// </summary>
         [Fact]
         public void Call_Log_with_message()
         {
-            var mockMessageLogger = new Mock<IMessageLogger>();
-            var sut = new Logger(mockMessageLogger.Object);
-
-            sut.Log("appears on console");
-            mockMessageLogger.Verify(a => a.Log(It.IsAny<string>()));
+            throw new NotImplementedException();
         }
     }
 }
