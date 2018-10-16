545bfefa1106094da0bbb2b8a501850083efa9f4
David Theunissen <davetheunissen@gmail.com>
2018-10-16 15:14:59 +1300

    basic tests and README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# IAGTechSpecInterview
+# IAG Tech Spec Interview
+
+Please fork and send this test back to us.
+
+## Overview
+
+This is a short refactoring exercise.
+
+The class IAGTechSpecInterview.Logger violates the Open Close Principle.
+
+Refactor this class so that message logging types are passed in when constructing IAGTechSpecInterview.Logger, and write a test to ensure IAGTechSpecInterview.Logger.Log is being called with a single string parameter.
+
+## Notes
+
+You don't need to install an IOC package.
+
+We are just looking to see how you refactor and write tests
+
+Xunit and MOQ have already been installed in the solution. If you have a different preferred mocking or fakes framework, then go ahead and use that :)
--- src/IAGTechSpecInterview/Logger.cs
+++ src/IAGTechSpecInterview/Logger.cs
@@ -2,7 +2,43 @@
 
 namespace IAGTechSpecInterview
 {
+    ///// <summary>
+    ///// Refactor this method to use the Open Close Principle
+    ///// </summary>
+    //public class Logger
+    //{
+    //    public void Log(string message, LogType logType)
+    //    {
+    //        switch (logType)
+    //        {
+    //            case LogType.Console:
+    //                Console.WriteLine(message);
+    //                break;
+    //            case LogType.File:
+    //                // Code to send message to printer
+    //                break;
+    //        }
+    //    }
+    //}
+    //public enum LogType
+    //{
+    //    Console,
+    //    File
+    //}
     public class Logger
     {
+        IMessageLogger _messageLogger;
+        public Logger(IMessageLogger messageLogger)
+        {
+            _messageLogger = messageLogger;
+        }
+        public void Log(string message)
+        {
+            _messageLogger.Log(message);
+        }
+    }
+    public interface IMessageLogger
+    {
+        void Log(string message);
     }
 }
